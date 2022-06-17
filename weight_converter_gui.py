01c13b9618e11b621bbe50d819ef9a317c277d93
Sarath P M <sarath2375@gmail.com>
2022-06-17 20:09:09 +0530

    Add a py program

---
+++ weight_converter_gui.py
@@ -0,0 +1,50 @@
+'''
+Requirements
+
+# pip install tk'
+'''
+
+# import necessary py modules
+from tkinter import *
+
+# creating a GUI Window
+window = Tk()
+window.title("Weight Converter")
+
+# function to convert from kg
+def from_kg():
+    gram = float(e2_value.get())*1000
+    pound = float(e2_value.get())*2.20462
+    ounce = float(e2_value.get())*35.274
+    t1.delete("1.0",END)
+    t1.insert(END, gram)
+    t2.delete("1.0", END)
+    t2.insert(END, pound)
+    t3.delete("1.0", END)
+    t3.insert(END, ounce)
+
+# GUI 
+e1 = Label(window, text="Enter the weight in KG")
+e2_value = StringVar()
+e2 = Entry(window, textvariable=e2_value)
+e3 = Label(window, text="Gram")
+e4 = Label(window, text="Pound")
+e5 = Label(window, text="Ounce")
+
+t1 = Text(window, height=5, width=30)
+t2 = Text(window, height=5, width=30)
+t3 = Text(window, height=5, width=30)
+
+b1 = Button(window, text="Convert", command=from_kg)
+
+e1.grid(row=0, column=0)
+e2.grid(row=0, column=1)
+e3.grid(row=1, column=0)
+e4.grid(row=1, column=1)
+e5.grid(row=1, column=2)
+t1.grid(row=2, column=0)
+t2.grid(row=2, column=1)
+t3.grid(row=2, column=2)
+b1.grid(row=0, column=2)
+
+window.mainloop()
