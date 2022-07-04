9e4d1d1975adb9253114c059f298faf5fc19bed0
Sarath P M <sarath2375@gmail.com>
2022-07-04 15:15:39 +0530

    Added a py program

---
+++ fidget_spinner.py
@@ -0,0 +1,55 @@
+
+'''
+Requirements
+
+# pip install turtle
+'''
+
+from turtle import *
+
+state= {'turn':0 }
+
+def spin():
+	clear()
+	angle = state['turn']/10
+	right(angle)
+	forward(100)
+	dot(120, 'red')
+	back(100)
+	"second dot"
+	right(120)
+	forward(100)
+	dot(120, 'blue')
+	back(100)
+	"third dot"
+	right(120)
+	forward(100)
+	dot(120, 'green')
+	back(100)
+	right(120)
+	update()
+
+def animate():
+	if state['turn']>0:
+		state['turn']-=1
+	spin()
+	ontimer(animate, 20)
+	
+def flick():
+	state['turn']+=40
+
+
+color('blue')
+write('Hello!')
+
+setup(600, 400, 370, 0)
+bgcolor("black")
+tracer(False)
+width(35)
+color("white")
+onkey(flick,'space')
+listen()
+animate()
+done()
+
+# Press "Space" to spin the fidget spinner.
